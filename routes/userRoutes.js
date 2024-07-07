6e3c25473c5e488df3ce04b74987f6f63ee64641
NocIcoN <90994680+NocIcoN@users.noreply.github.com>
2024-07-07 11:29:32 +0700

    add models and routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,34 +1,34 @@
 const express = require('express');
-const User = require('../models/user');
+const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
-// Register User
+// Registrasi
 router.post('/register', async (req, res) => {
+  const { username, email, password } = req.body;
   try {
-    const { username, email, password } = req.body;
-    const newUser = new User({ username, email, password });
-    await newUser.save();
-    res.status(201).send('User registered successfully');
+    const user = new User({ username, email, password });
+    await user.save();
+    res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-// Login User
+// Login
 router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
   try {
-    const { username, password } = req.body;
     const user = await User.findOne({ username });
-    if (!user) return res.status(404).send('User not found');
+    if (!user) return res.status(404).json({ message: 'User not found' });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).send('Invalid credentials');
+    if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.status(200).json({ token });
+    res.status(200).json({ token, message: 'Logged in successfully' });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const userRoutes = require('./routes/userRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+const testRoutes = require('./routes/testRoutes');
 
 dotenv.config();
-
 const app = express();
-app.use(express.json()); // Middleware to parse JSON
+app.use(express.json());
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DATABASE_URL)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
-const userRouter = require('./routes/userRoutes');
-app.use('/api/users', userRouter);
+app.use('/api/users', userRoutes);
+app.use('/api/admins', adminRoutes);
+app.use('/api/tests', testRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
